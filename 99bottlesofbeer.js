cdd290864c3ad9d78f74dbe7ab5441645add6f2f
ybinstock <ybinstock@gmail.com>
2014-09-17 21:23:22 -0700

    first

---
+++ 99bottlesofbeer.js
@@ -0,0 +1,12 @@
+var bottles = 99
+
+
+
+while (bottles > 0){
+	console.log (bottles+" "+"bottles of beer on the wall" + " "+ bottles+ "of beer"+"knock one down, pass it around"+" "+bottles+"bottles of beer on the wall")
+	var bottles = bottles - 1
+};
+
+
+
+console.log("no more beer!");
